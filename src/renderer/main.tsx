12415ac56e2f3ec4ab2d82a997f493cfe4ceb5a9
CoWork OS <hello@cowork-os.com>
2026-05-18 09:16:54 +0100

    Load approval dialog styles in the main renderer

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -4,6 +4,7 @@ import "./react-refresh-ignored-exports";
 import { App } from "./App";
 import "./i18n"; // Initialize i18n before rendering
 import "./styles/index.css";
+import "./components/right-panel.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
